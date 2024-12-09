7fc62f12c3d15fb7a5fe031458be3673a6736454
Lanagramme <lborromee16@gmail.com>
2024-12-09 16:59:58 +0100

    spells

--- data/spells.py
+++ data/spells.py
@@ -23,19 +23,23 @@ def direct_damage(spell, targets):
 #   return targets
 
 # ====== [ Fireball ] ======
-Spells["Fireball"] = Spell('Fireball', 8, "circle", 3, 1)
-
-def fireball_effect(target):
-  direct_damage(Spells['Fireball'], target)
-
-Spells['Fireball']["effect"] = fireball_effect
-
+Fireball = Spell('Fireball', 8, "circle", 3, 1)
+def fireball_effect(target): direct_damage(Fireball, target)
+Fireball["effect"] = fireball_effect
+Spells["Fireball"] = Fireball
 
 # ====== [ Water stream ] ======
-Spells['Water stream'] = Spell('Water stream', 4, "line", 3, 3)
- 
-def water_stream_effect(target):
-  direct_damage(Spells['Water stream'], target)
 
-Spells['Water stream']["effect"] = water_stream_effect
+Water_stream = Spell('Water stream', 4, "line", 3, 3)
+def water_stream_effect(target): direct_damage(Water_stream, target)
+Water_stream["effect"] = water_stream_effect
+Spells['Water stream'] = Water_stream
 
+# ====== [ Spark ] ======
+# Cost: 1F 1PA
+# all fire token of the target explode adding 2 dmg by token 
+Spark = Spell('Spark', 3, "circle", 2, 0)
+def spark_effect(target): 
+  direct_damage(Spark, target)
+Spark["effect"] = spark_effect
+Spells["Spark"] = Spark
